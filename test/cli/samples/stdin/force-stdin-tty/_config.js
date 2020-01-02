02a930e46c770011ca2dff053ac34270cb89ad2a
Lukas Taegert-Atkinson <lukas.taegert-atkinson@tngtech.com>
2020-01-02 11:23:14 +0100

    Switch stdin to a regular two-state flag

--- test/cli/samples/stdin/force-stdin-tty/_config.js
+++ test/cli/samples/stdin/force-stdin-tty/_config.js
@@ -1,4 +1,5 @@
 module.exports = {
+	solo: true,
 	description: 'allows forcing stdin input on TTY interfaces via option',
 	skipIfWindows: true,
 	command: `echo "console.log('PASS');" | ./wrapper.js -f es --stdin`
